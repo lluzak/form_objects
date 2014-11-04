7c9631526024a90c6c52c5ac3944c41c960d4b8b
Piotr Niełacny <piotr.nielacny@gmail.com>
2014-11-05 00:02:44 +0100

    Add support for date multi-parameter
    From now ParamsConverter can convert params generated by rails helpers
    to Virtus friendly format.

--- lib/form_objects/params_converter.rb
+++ lib/form_objects/params_converter.rb
@@ -1,3 +1,6 @@
+require 'form_objects/params_converter/date_converter'
+require 'form_objects/params_converter/collection_converter'
+
 module FormObjects
   class ParamsConverter
     def initialize(params)
@@ -5,44 +8,10 @@ module FormObjects
     end
 
     def params
-      convert_attributes_to_array(@params)
-    end
+      params = CollectionConverter.new(@params).params
+      params = DateConverter.new(params).params
 
-    def convert_attributes_to_array(object)
-      return object unless object.respond_to?(:each_pair)
-
-      object.inject({}) { |hash, attributes|
-        key, value = attributes.first, attributes.last
-        value      = value.to_a.sort.map { |attributes| attributes.last } if candidate_for_conversion?(key, value)
-        hash[key]  = convert_attributes_to_array(value)
-
-        hash
-      }
-    end
-
-    private
-
-    def candidate_for_conversion?(key, value)
-      attribute_key?(key) and value.is_a?(Hash) and incrementing_sequence?(value.keys)
-    rescue ArgumentError
-      false
-    end
-
-    def generate_sequence_from(keys)
-      keys.map { |key| Integer(key) }.sort
-    end
-
-    def sequence_to(max)
-      (0..max).to_a
-    end
-
-    def incrementing_sequence?(keys)
-      sequence = generate_sequence_from(keys)
-      sequence == sequence_to(sequence.max)
-    end
-
-    def attribute_key?(key)
-      key =~ /_attributes$/
+      params
     end
   end
 end
